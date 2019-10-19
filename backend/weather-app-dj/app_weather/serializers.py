06857de692bdaf6afd8e3b74e2211275aea97221
Szymon Gruszczyński <gruszczynski01@gmail.com>
2019-10-19 20:24:16 +0200

    cleaning

--- backend/weather-app-dj/app_weather/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from app_weather.models import ExampleModel
-
-class ExampleModelSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ExampleModel
-        fields = ('firstname', 'lastname')
