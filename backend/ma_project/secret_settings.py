b22cf8180636b02e757a99c89b0136f75d9040db
kingus <kingulka.a@gmail.com>
2019-10-20 14:49:51 +0200

    Integration 3

---
+++ backend/ma_project/secret_settings.py
@@ -0,0 +1,3 @@
+MA_SECRET_KEY=['cv65yju762#+z)e2_@gu@=ayc982@bdl_f7_$p-ll*oxz^y=p$']
+MA_WEATHER_API_KEY=['5aeb63d1ec3917eba36a8fd576f601b6']
+MA_NEWS_API_KEY=['ea07f252be304105928c2de1e8e393de']
---
+++ backend/ma_project/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include, re_path
+from ma_app.views import FrontendRenderView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/', include('ma_app.api.urls')), 
+    re_path(r'(?P<path>.*)', FrontendRenderView.as_view(), name = "home")
+]
