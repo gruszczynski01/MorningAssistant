2e3450af6cf3f513b055ab7bbdb13054e0c305be
Szymon Gruszczyński <gruszczynski01@gmail.com>
2019-10-22 20:21:54 +0200

    cdkDropList testing

--- frontend/my-app/src/app/home/home.component.ts
+++ frontend/my-app/src/app/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { MainService } from '../services/main.service';
 import { HttpClient } from '@angular/common/http';
 import { faUserAlt } from '@fortawesome/free-solid-svg-icons';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { moveItemInArray, CdkDragDrop } from '@angular/cdk/drag-drop';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -9,13 +11,25 @@ import { faUserAlt } from '@fortawesome/free-solid-svg-icons';
 })
 export class HomeComponent implements OnInit {
   faUserAlt = faUserAlt;
+  faHome = faHome;
   dataFromService: any;
+  movies = [
+    'NEWS',
+    'POGODA',
+    'TO DO LISTA',
+    'KALENDAR'
+  ];
+
+
   constructor(private mainService: MainService, private http: HttpClient) { }
 
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
+  }
 
 
   ngOnInit() {
-    this.mainService.getDataFromDB('Legionowo').subscribe(data => {
+    this.mainService.getDataFromDB('Łuków').subscribe(data => {
       this.dataFromService = data as [any];
     });
 
