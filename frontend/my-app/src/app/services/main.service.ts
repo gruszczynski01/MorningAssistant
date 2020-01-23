e81e60463cc8535bc898cf222b17ace8f1c102f3
kingus <kingulka.a@gmail.com>
2020-01-23 13:41:06 +0100

    tiles url changed in frontend

--- frontend/my-app/src/app/services/main.service.ts
+++ frontend/my-app/src/app/services/main.service.ts
@@ -1,51 +1,52 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
 
-const user = '';
-const password = '';
+const user = "";
+const password = "";
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
-
 export class MainService {
   // tslint:disable-next-line: variable-name
-  base_url = 'http://localhost:8000';
-  constructor(private http: HttpClient) { }
+  base_url = "http://localhost:8000";
+  constructor(private http: HttpClient) {}
 
   // tslint:disable-next-line: no-shadowed-variable
   getToken(username: string, password: string): Observable<any> {
     // tslint:disable-next-line: object-literal-key-quotes
-    const body = { 'username': username, 'password': password };
-    return this.http.post(this.base_url + '/api/v1/users/obtain-token', body);
+    const body = { username: username, password: password };
+    return this.http.post(this.base_url + "/api/v1/users/obtain-token", body);
   }
 
   getUserInfo(): Observable<any> {
     // tslint:disable-next-line: object-literal-key-quotes
-    const headers = { 'Authorization': 'Token ' + localStorage.getItem('token') };
+    const headers = { Authorization: "Token " + localStorage.getItem("token") };
     const httpHeaders = new HttpHeaders(headers);
-    return this.http.get(this.base_url + '/api/v1/users/me', { headers });
+    return this.http.get(this.base_url + "/api/v1/users/me", { headers });
   }
   getUserTiles(): Observable<any> {
     // tslint:disable-next-line: object-literal-key-quotes
-    const headers = { 'Authorization': 'Token ' + localStorage.getItem('token') };
+    const headers = { Authorization: "Token " + localStorage.getItem("token") };
     const httpHeaders = new HttpHeaders(headers);
-    return this.http.get(this.base_url + '/api/v1/users/tiles', { headers });
+    return this.http.get(this.base_url + "/api/v1/users/me/tiles", { headers });
   }
 
   getWeatherData(city: string): Observable<any> {
     // tslint:disable-next-line: object-literal-key-quotes
-    const headers = { 'Authorization': 'Token ' + localStorage.getItem('token') };
+    const headers = { Authorization: "Token " + localStorage.getItem("token") };
     const httpHeaders = new HttpHeaders(headers);
-    return this.http.get(this.base_url + '/api/v1/weather/' + city, { headers });
+    return this.http.get(this.base_url + "/api/v1/weather/" + city, {
+      headers
+    });
   }
 
   getNewsData(category: string): Observable<any> {
     // tslint:disable-next-line: object-literal-key-quotes
-    const headers = { 'Authorization': 'Token ' + localStorage.getItem('token') };
+    const headers = { Authorization: "Token " + localStorage.getItem("token") };
     const httpHeaders = new HttpHeaders(headers);
-    return this.http.get(this.base_url + '/api/v1/news/' + category, { headers });
+    return this.http.get(this.base_url + "/api/v1/news/" + category, {
+      headers
+    });
   }
-
-
 }
